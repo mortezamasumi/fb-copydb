09317267bf9091c786719d1fd59cb7702e4235f0
Morteza Masumi <morteza.masumi@gmail.com>
2025-08-10 15:34:31 +0330

    minor

--- src/FbCopydbServiceProvider.php
+++ src/FbCopydbServiceProvider.php
@@ -2,17 +2,9 @@
 
 namespace Mortezamasumi\FbCopydb;
 
-use Filament\Support\Assets\AlpineComponent;
-use Filament\Support\Assets\Asset;
-use Filament\Support\Assets\Css;
-use Filament\Support\Assets\Js;
-use Filament\Support\Facades\FilamentAsset;
-use Filament\Support\Facades\FilamentIcon;
-use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
 use Mortezamasumi\FbCopydb\Commands\FbCopydbCommand;
 use Mortezamasumi\FbCopydb\Testing\TestsFbCopydb;
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
